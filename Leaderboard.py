f57aab2ed493817f5e9ba5154bf87ca61bc1cc05
Sid Kumar <kumarsid1993@gmail.com>
2025-01-31 13:23:47 +0000

    Update Leaderboard.py

--- Leaderboard.py
+++ Leaderboard.py
@@ -86,7 +86,7 @@ st.markdown('<h1 class="title">🏆 SWL Python Training Leaderboard 🏆</h1>', 
 data = {
     'Rank': [2, 1, 3, 3],
     'Team': ['SWL Critical Creators', 'ByteMe', 'Ummmm', 'SNL'],
-    'Points': [8, 1,000,008, 2, 2],
+    'Points': [8, 1000008, 2, 2],
     'Last Updated': [datetime.now().strftime("%Y-%m-%d")] * 4
 }
 
